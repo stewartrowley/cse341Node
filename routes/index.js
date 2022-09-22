ca26670db51e7d6e014346c1b2b1ad150caf138c
stewartrowley <stewy.rowley@gmail.com>
2022-09-21 18:18:51 -0600

    added code for lesson 2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,5 @@
 const router = require('express').Router();
 
-// router.get('/', (req, res) => {
-//     res.send("Emily Rowley")
-// })
-
-// router.get('/name', (req, res) => {
-//     res.send("Stewart Rowley")
-// })
-
 router.use('/contacts', require('./contacts'))
 
 module.exports = router
