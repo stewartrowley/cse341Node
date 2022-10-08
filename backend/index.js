6bf57544a415209e6b4e0df8c1dcdcc030a499af
stewartrowley <stewy.rowley@gmail.com>
2022-10-08 16:07:51 -0600

    Changes to the backend and frontend of the code

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./database/connect');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Methods', 'GET','POST', 'PUT', 'DELETE', 'OPTIONS')
     next();
   })
   .use('/', require('./routes'));
